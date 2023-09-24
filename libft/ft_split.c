07cbd656852d291c147a4241305eb889373a0145
330namajnoon <sina.cappac@gmail.com>
2023-09-24 03:37:18 +0200

    split finish1

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,20 +6,56 @@
 /*   By: simajnoo <simajnoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 18:01:23 by simajnoo          #+#    #+#             */
-/*   Updated: 2023/09/22 22:13:05 by simajnoo         ###   ########.fr       */
+/*   Updated: 2023/09/24 03:21:20 by simajnoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	*lens(const char *s, char c)
+struct	s_reslen {
+	char	**res;
+	int		*len;
+};
+
+void	set_vars(int *vars)
 {
-	int	vars[6];
+	vars[1] = 0;
+	vars[2] = 1;
+	vars[3] = 0;
+	vars[4] = 0;
+}
+
+int	*set_lens(int *vars, int *lens, const char *s, char c)
+{
+	if (vars[5])
+		vars[0]--;
+	if (!lens)
+		return (FT_NULL);
+	else
+		lens[0] = vars[0];
+	set_vars(vars);
+	while (s[vars[1]] == c && s[vars[1]] != 0)
+		vars[1]++;
+	while (s[vars[1]] != 0)
+	{
+		if (s[vars[1]] == c)
+			vars[3] = 1;
+		else if (vars[3] == 1)
+		{
+			vars[2]++;
+			vars[3] = 0;
+		}
+		if (vars[3] == 0)
+			lens[vars[2]]++;
+		vars[1]++;
+	}
+	return (lens);
+}
+
+int	*lens(int *vars, const char *s, char c)
+{
 	int	*lens;
 
-	vars[5] = 0;
-	vars[0] = 1;
-	vars[1] = 0;
 	while (s[vars[1]] == c && s[vars[1]] != 0)
 		vars[1]++;
 	if (s[vars[1]] == 0)
@@ -38,68 +74,46 @@ int	*lens(const char *s, char c)
 		vars[1]++;
 	}
 	lens = (int *)calloc(vars[0] + 1, sizeof(int));
-	if(vars[5])
-		vars[0]--;
-	if (!lens)
+	if (!set_lens(vars, lens, s, c))
 		return (FT_NULL);
-	else
-		lens[0] = vars[0];
-	vars[1] = 0;
-	vars[2] = 1;
-	vars[3] = 0;
-	vars[4] = 0;
-	while (s[vars[1]] == c && s[vars[1]] != 0)
-		vars[1]++;
-	while (s[vars[1]] != 0)
-	{
-		if (s[vars[1]] == c)
-			vars[3] = 1;
-		else if(vars[3] == 1)
-		{
-			vars[2]++;
-			vars[3] = 0;
-		}
-		if (vars[3] == 0)
-			lens[vars[2]]++;
-		vars[1]++;
-	}
 	return (lens);
 }
 
-void	setRes(char **res, int *len, int *vars, const char *s, char c)
+void	set_res(struct s_reslen rl, int *vs, const char *s, char c)
 {
-	vars[0] = -1;
-	vars[3] = 0;
-	vars[1] = 0;
-	while (s[vars[3]] == c)
-		vars[3]++;
-	while (++vars[1] <= len[0])
+	vs[0] = -1;
+	vs[3] = 0;
+	vs[1] = 0;
+	while (s[vs[3]] == c)
+		vs[3]++;
+	while (++vs[1] <= rl.len[0])
 	{
-		vars[2] = -1;
-		res[++vars[0]] = (char *)malloc((len[vars[1]] + 1) * sizeof(char));
-		while (++vars[2] < len[vars[1]])
-			res[vars[0]][vars[2]] = s[vars[3]++];
-		res[vars[0]][vars[2]] = 0;
-		while (s[vars[3]] == c)
-			vars[3]++;
+		vs[2] = -1;
+		rl.res[++vs[0]] = (char *)malloc((rl.len[vs[1]] + 1) * sizeof(char));
+		while (++vs[2] < rl.len[vs[1]])
+			rl.res[vs[0]][vs[2]] = s[vs[3]++];
+		rl.res[vs[0]][vs[2]] = 0;
+		while (s[vs[3]] == c)
+			vs[3]++;
 	}
-	res[++vars[0]] = FT_NULL;
-	free(len);
+	rl.res[++vs[0]] = FT_NULL;
+	free(rl.len);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**res;
-	int		*len;
-	int		vars[4];
+	struct s_reslen	res_len;
+	int				vars[6];
 
-	len = lens(s, c);
-	if (!len)
+	vars[5] = 0;
+	vars[0] = 1;
+	vars[1] = 0;
+	res_len.len = lens(vars, s, c);
+	if (!res_len.len)
 		return (FT_NULL);
-	res = (char **)malloc((len[0] + 1) * sizeof(char*));
-	if (!res)
+	res_len.res = (char **)malloc((res_len.len[0] + 1) * sizeof(char *));
+	if (!res_len.res)
 		return (FT_NULL);
-	setRes(res, len, vars, s, c);
-	return (res);
+	set_res(res_len, vars, s, c);
+	return (res_len.res);
 }
-
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: simajnoo <simajnoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 16:07:01 by simajnoo          #+#    #+#             */
-/*   Updated: 2023/09/22 22:01:59 by simajnoo         ###   ########.fr       */
+/*   Updated: 2023/09/24 03:36:18 by simajnoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int	main(int argn, char *argv[])
 
 	dst = ft_split(argv[1], argv[2][0]);
 	i = -1;
-
+	printf("%d\n",dst[2] == FT_NULL);
+	printf("%d\n",ft_memcmp(dst[0], "sina", 4));
+	
 	//printf("%s\n",dst[0]);
 	while (dst[++i] != FT_NULL)
 	{
