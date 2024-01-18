28a12c8a1872ef41d81d55ec01f9543d8d43a8a0
330namajnoon <sina.cappac@gmail.com>
2024-01-18 22:11:11 +0100

    finished

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: simajnoo <simajnoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 18:13:56 by simajnoo          #+#    #+#             */
-/*   Updated: 2024/01/14 20:12:54 by simajnoo         ###   ########.fr       */
+/*   Updated: 2024/01/18 21:59:49 by simajnoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	main(int argn, char *argv[])
 	// ft_printf("%c | %s | %p | %d | %i | %u | %x | %X\n", 'a', "Hola Mundo", str, 2024, 42, -110, 2025, 2025);
 	// printf("%c | %s | %p | %d | %i | %u | %x | %X\n", 'a', "Hola Mundo", str, 2024, 42, -110, 2025, 2025);
 	//char *p = ft_strdup("Hola mundo");
-	ft_printf("%d\n", ft_printf("| %X |", 99));
-	printf("%d\n", printf("| %X |", 99));
+	ft_printf("%d\n", ft_printf("| %p |", (void *)-14523));
+	printf("%d\n", printf("| %p |", (void *)-14523));
 	//printf("%d\n", printf("NULL %s NULL\n", NULL));
 
 	return (0);
--- ft_printf/putpoiterfd.c
+++ ft_printf/putpoiterfd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   putpoiterfd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: simajnoo <simajnoo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 12:03:10 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/16 12:08:48 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/18 22:09:38 by simajnoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,25 @@
 
 int	putpointerfd(void *ptr, char *base, int fd)
 {
-	char			buffer[16];
+	char			buffer[18];
 	unsigned long	address;
 	int				i;
 	int				digit;
 
 	address = (unsigned long)ptr;
-	i = 15;
+
+	i = 17;
 	digit = 0;
-	if (!ptr)
-		return (write(fd, "(nil)", 5));
 	while (address != 0 && i >= 0)
 	{
 		digit = address % 16;
 		buffer[i--] = base[digit];
 		address /= 16;
 	}
+	if (!ptr)
+		buffer[i--] = '0';
 	buffer[i--] = 'x';
 	buffer[i] = '0';
-	write(fd, &buffer[i], 16 - i);
-	return (16 - i);
+	write(fd, &buffer[i], 18 - i);
+	return (18 - i);
 }
